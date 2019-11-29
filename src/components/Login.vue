6d91ba9f59623bf71043b36ef735a33731e0abdc
tobeng <294571638@qq.com>
2019-11-30 00:35:22 +0800

    init file

---
+++ src/components/Login.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <!-- 登录头像 -->
+            <div class="avatar_box">
+                <img src="../assets/logo.png" />
+            </div>
+            <!-- 登录表单 -->
+            <el-form :model="loginFrom" label-width="0px" class="login_form">
+                <!-- 用户名 -->
+                <el-form-item label="">
+                    <el-input v-model="loginFrom.username" prefix-icon="iconfont icon-user"></el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                <el-form-item label="">
+                    <el-input v-model="loginFrom.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+                </el-form-item>
+                <!-- 按钮区域 -->
+                <el-form-item class="btns">
+                    <el-button type="primary">登录</el-button>
+                    <el-button type="info">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            loginFrom:{
+                username:"admin",
+                password:"admin@2018"
+            }
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+
+    .login_box{
+        width: 450px;
+        height: 300px;
+        background-color: #FFFFFF;
+        border-radius: 3px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+
+        .avatar_box{
+            height: 130px;
+            width: 130px;
+            border: 1px solid #eee;
+            border-radius: 50%;
+            padding: 10px;
+            box-shadow: 0 0 10px #ddd;
+            left: 50%;
+            position: absolute;
+            transform: translate(-50%, -50%);
+            background-color: #FFFFFF;
+            img{
+                width: 100%;
+                height: 100%;
+                border-radius: 50%;
+                background-color: #eee;
+            }
+        };
+        .login_form{
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            padding: 0 20px;
+            box-sizing: border-box;
+            .btns{
+                display: flex;
+                justify-content: flex-end;
+            }
+        }
+        
+    }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+// 引入全局样式
+import './assets/css/global.css'
+// 引入第三方样式
+import './assets/fonts/iconfont.css'
+// 引入elementUI样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
