ff69dd5d79b0a8586d194c713f46ed27c9e8e7a7
tobeng <294571638@qq.com>
2019-12-09 00:02:24 +0800

    完成数据列表展示

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -18,11 +18,108 @@
           <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
+      <el-row>
+        <el-table :data="userList" style="width: 100%" border stripe>
+          <el-table-column type="index" label="编号"></el-table-column>
+          <el-table-column prop="username" label="姓名"></el-table-column>
+          <el-table-column prop="age" label="年龄"></el-table-column>
+          <el-table-column prop="address" label="地址"></el-table-column>
+          <el-table-column prop="msg_status" label="状态">
+            <!-- 作用域插槽 -->
+            <template slot-scope="scope">
+              <el-switch
+                v-model="scope.row.msg_status"
+                active-color="#13ce66"
+                inactive-color="#ff4949"
+                @change="useStatusChang(scope.row)"
+              ></el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column prop label="操作">
+            <template slot-scope="scope">
+              <el-tooltip effect="dark" content="修改" placement="top" :enterable="false">
+                <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+              </el-tooltip>
+              <el-tooltip effect="dark" content="删除" placement="top" :enterable="false">
+                <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+              </el-tooltip>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页 -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pageNum"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="queryInfo.pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
+      </el-row>
     </el-card>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 获取用户列表参数对象
+      queryInfo: {
+        query: '',
+        pageNum: 1,
+        pageSize: 20
+      },
+      userList: [
+        {
+          id:1,
+          username: '张三',
+          age: 20,
+          address: '上海徐汇区',
+          msg_status: true
+        }
+      ],
+      total: 0,
+      pageNum: 1,
+      pageSize: 20
+    }
+  },
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      const { data: res } = this.$http.get('users', { params: this.queryInfo })
+      if (res.meta.status !== 200) {
+        this.$message.error('获取用户信息失败！')
+      }
+      this.userList = res.data.users
+      this.total = res.data.total
+      this.pageNum = res.data.pageNum
+      this.pageSize = res.data.pageSize
+    },
+    // 监听 pagesize 改变事件
+    handleSizeChange(newsize){
+      this.queryInfo.pageSize = newsize; 
+      this.getUserList();
+    },
+    // 监听页码值改变事件
+    handleCurrentChange(newpage){
+      this.queryInfo.pageNum = newpage;
+      this.getUserList();
+    },
+    // 监听 switch 触发事件
+    useStatusChang(userInfo){
+      const {data: res} = this.$http.put(`users/${userInfo.id}/status/${userInfo.msg_status}`);
+      if(res.status !== 200){
+        userInfo.msg_status = !userInfo.msg_status;
+        this.$message.error("更新状态失败！");
+        return;
+      }
+      this.$message.success("更新状态成功！");
+    }
+  }
+}
 </script>
 <style lang="less" scoped>
 </style>
